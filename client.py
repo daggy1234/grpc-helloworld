2e30d176ac0ad9e4fce22c0cd391bf7a78f0fc30
Ubuntu <ubuntu@ip-172-31-18-70.ec2.internal>
2023-08-01 17:31:35 +0000

    grpc benchmarks

--- client.py
+++ client.py
@@ -23,6 +23,32 @@ import rpc_hello_pb2_grpc
 import time
 import csv
 
+# from rpc_echo_python_client import Connection, HelloRequest
+# import time
+# import csv
+# from random import randbytes, randrange
+
+# c = Connection("172.31.27.226:5000")
+
+# for pow in range(7):
+#     length = 10 ** pow
+#     print(length)
+#     time_l = []
+#     for i in range(1000):
+#         req_str = "a" * length
+#         start = time.time()
+#         req = HelloRequest(req_str)
+#         o = c.say_hello(req)
+#         end = time.time()
+#         diff = end - start
+#         time_l.append([str(i), diff])
+
+#     with open(f"./data/mrpc_data_{length}.csv", "w") as f:
+#         csvwriter = csv.writer(f)
+#         csvwriter.writerows(time_l)
+
+# print(sum(time_l)/len(time_l))
+
 
 def run():
     # NOTE(gRPC Python Team): .close() is possible on a channel and should be
@@ -31,13 +57,20 @@ def run():
     print("Will try to greet world ...")
     with grpc.insecure_channel('172.31.27.226:50051') as channel:
         stub = rpc_hello_pb2_grpc.GreeterStub(channel)
-        time_l = []
-        for i in range(3000):
-          start = time.time()
-          response = stub.SayHello(rpc_hello_pb2.HelloRequest(name=f'test message number {i}'))
-          end  = time.time()
-          diff = end - start
-          time_l.append([str(i), diff])
+        for pow in range(7):
+            length = 10 ** pow
+            time_l = []
+            for i in range(1000):
+                req_str = "a" * length
+                start = time.time()
+                response = stub.SayHello(rpc_hello_pb2.HelloRequest(name=req_str))
+                end  = time.time()
+                diff = end - start
+                time_l.append([str(i), diff])
+            with open(f"./data/mrpc_data_{length}.csv", "w") as f:
+                csvwriter = csv.writer(f)
+                csvwriter.writerows(time_l)
+            
     print("Greeter client received: " + response.message)
     with open("grpc_data.csv", "w") as f:
         csvwriter = csv.writer(f)
